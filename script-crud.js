42e34618b7a8c9bbc780d7107316e1c6fcd728c4
Nillton Pimenta <njqp@icomp.ufam.edu.br>
2025-03-14 16:37:19 -0400

    Edit tasks

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,10 @@ const ulTarefas = document.querySelector(".app__section-task-list");
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
+function atualizarTarefas() {
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
+
 function criarElementoTarefa(tarefa) {
     const li = document.createElement("li");
     li.classList.add("app__section-task-list-item");
@@ -26,6 +30,17 @@ function criarElementoTarefa(tarefa) {
 
     const botao = document.createElement("button");
     botao.classList.add("app_button-edit");
+
+    botao.onclick = () => {
+        const novaDescricao = prompt("Qual o novo nome da tarefa?");
+
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao;
+            tarefa.descricao = novaDescricao;
+            atualizarTarefas();
+        }
+    };
+
     const imagemBotao = document.createElement("img");
     imagemBotao.setAttribute("src", "/imagens/edit.png");
     botao.append(imagemBotao);
@@ -51,7 +66,7 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
 
-    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+    atualizarTarefas();
 
     textArea.value = "";
     formAdicionarTarefa.classList.add("hidden");
